8d729c47abda27b548ee54d9fe0a96570cd19d38
webNekit <mochalov-hunter@mail.ru>
2024-01-18 17:58:19 +0300

    Написали роутер для маршрутизаци

--- test-app/routes/web.php
+++ test-app/routes/web.php
@@ -14,10 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 // Открытие главной страницы
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 // Открытие страницы о нас
-Route::get('/about', function () {
-    return view('about');
-});
+//Route::get('/about', function () {
+//    return view('about');
+//});
+
+Route::get('/', [\App\Http\Controllers\FrontendController::class, 'index'])->name('home');
+Route::get('/about', [\App\Http\Controllers\FrontendController::class, 'about'])->name('about');
+
