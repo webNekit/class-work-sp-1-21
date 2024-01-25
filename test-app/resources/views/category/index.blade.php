313b8c493819f7e1b1020cef765db0c9f7f368a9
webNekit <mochalov-hunter@mail.ru>
2024-01-25 18:27:10 +0300

    Сделали страницу category/index и category/create

---
+++ test-app/resources/views/category/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layout.master')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <a href="#!" class="btn btn-primary">Новая категория</a>
+        </div>
+        <div class="row">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Название поста</th>
+                    <th scope="col">Описание поста</th>
+                    <th scope="col">Изображение</th>
+                    <th scope="col">Действие</th>
+                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"></th>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
